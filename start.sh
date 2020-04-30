5dcdb140769220060747857ac3cc9d14b9a963a2
Rudhra Raveendran <rooday@bu.edu>
2020-04-30 15:52:08 +0000

    Changed for my purposes

--- start.sh
+++ start.sh
@@ -1,38 +1,10 @@
 #!/bin/bash
 
-# git clone https://github.com/AMD-NICK/docker-garrysmod-server.git gmod-docker
-# cd gmod-docker
-# docker build -t gmod:1 .
-
-mkdir -p volume ; cd volume
-mkdir -p addons data luabin gmas
-touch sv.db
-
-chown 999:999 sv.db data/ gmas/
-
-if [ ! -d "darkrp" ]; then
-	echo "Installing DarkRP"
-	git clone https://github.com/FPtje/DarkRP.git darkrp
-fi
-
-
-if [ ! -d "addons/ulx_ulib" ]; then
-	echo "Installing Ulx and Ulib"
-
-	mkdir -p tmp/ulx_ulib
-	cd tmp
-
-	git clone 'https://github.com/TeamUlysses/ulx.git'
-	git clone 'https://github.com/TeamUlysses/ulib.git'
-
-	cp -r 'ulx/lua' ulx_ulib/
-	cp -r 'ulib/lua' ulx_ulib/
-
-	mv ulx_ulib/ ../addons
-
-	cd ..
-	rm -rf tmp
-fi
-
-cd .. # exit from /volume
-docker-compose up --force-recreate
+docker run -it -d --name gmod --restart always \
+	-p 27000-27015:27000-27015/udp -p 1200:1200/udp -p 27030-27039:27030-27039/tcp -p 27020:27020/udp -p 27015:27015/tcp \
+	-v $PWD/garrysmod/cfg:/gmodserv/garrysmod/cfg/ \
+	-v $PWD/garrysmod/addons:/gmodserv/garrysmod/addons/ \
+	gmod-server -port 27015 -tickrate 60 -maxplayers 24 \
+	+gamemode terrortown +map ttt_67thway_v6 \
+	+host_workshop_collection 1413001823 \
+	+sv_setsteamaccount 3B6C6D56043FD72170A69830108C092D
