14d2d95d21570bb81ac30bb3090053834c4f03a2
Rudhra Raveendran <rooday@bu.edu>
2020-05-23 13:39:44 -0400

    update

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ fi
 docker run -it -d --name gmod --restart always \
 	-p 27000-27015:27000-27015/udp -p 1200:1200/udp -p 27030-27039:27030-27039/tcp -p 27020:27020/udp -p 27015:27015/tcp \
 	-v $PWD/garrysmod2:/gmodserv/garrysmod/ \
-	gmod-server -port 27015 -tickrate 60 -maxplayers 24 \
+	gmod-server -port 27015 -tickrate 60 -maxplayers 16 \
 	+gamemode terrortown +map ttt_67thway_v6 \
 	+host_workshop_collection 1413001823 \
 	+sv_setsteamaccount $GSLT
